7e0849cc25a2413ee85a2e0017fa2d3a3690489e
David Axel Nikyèma <david-axel-wendbenedo.nikyema.1@ulaval.ca>
2021-10-16 13:13:53 +0000

    Initial commit

---
+++ src/main/java/bf/mavico/backendws/BackendWsApplication.java
@@ -0,0 +1,13 @@
+package bf.mavico.backendws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendWsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendWsApplication.class, args);
+    }
+
+}
